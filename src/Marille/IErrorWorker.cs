f6a079c87522afd364c81217435fb41fa35e1eb6
Manuel de la Pena <mandel@microsoft.com>
2024-08-29 13:16:30 -0400

    [Docs] Add intial docs.

--- src/Marille/IErrorWorker.cs
+++ src/Marille/IErrorWorker.cs
@@ -1,8 +1,24 @@
 namespace Marille;
 
+/// <summary>
+/// Interface to be implemented by a worker that will take care of handling the errors from IWorker consumers in a
+/// give channel.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public interface IErrorWorker<in T> : IDisposable, IAsyncDisposable where T : struct {
 
+	/// <summary>
+	/// Specifies if the worker should use a background thread to process the messages. If set to
+	/// true the implementation of the worker should be thread safe.
+	/// </summary>
 	public bool UseBackgroundThread { get; }
 
+	/// <summary>
+	///
+	/// </summary>
+	/// <param name="message"></param>
+	/// <param name="exception"></param>
+	/// <param name="token"></param>
+	/// <returns></returns>
 	public Task ConsumeAsync (T message, Exception exception, CancellationToken token = default);
 }
--- src/Marille/WorkerError.cs
+++
@@ -1,5 +0,0 @@
-namespace Marille;
-
-public class WorkerError {
-	public WorkerError(Type type, object worker, Exception? ex) {}
-}
