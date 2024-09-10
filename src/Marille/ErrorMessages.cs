bc94f6abb5caa717574174bd2c9fdbedb55ad2cf
Manuel de la Pena Saenz <mandel@microsoft.com>
2024-09-10 16:34:28 -0400

    [Logging] Add logging for the case in which workers throw.
    Add logging to know when a worker throws due to reasons that are out of
    our control.

--- src/Marille/ErrorMessages.cs
+++ src/Marille/ErrorMessages.cs
@@ -36,4 +36,18 @@ internal static partial class ErrorMessages {
 		Level = LogLevel.Error,
 		SkipEnabledCheck = true)]
 	internal static partial void LogCloseErrorTopicDoesNotExist (this ILogger logger, string topicName, Type topicType);
+	
+	[LoggerMessage(
+		Message = "ErrorWorker {ErrorWorker} failed to process message {Message} with {Exception} on {TopicName} of {TopicType}", 
+		Level = LogLevel.Error,
+		SkipEnabledCheck = true)]
+	internal static partial void LogErrorConsumerException (this ILogger logger, Type errorWorker, object message, 
+		Exception exception, string topicName, Type topicType);
+	
+	[LoggerMessage(
+		Message = "Worker {Worker} raised {Exception} OnChannelClose for {TopicName} of {TopicType}",
+		Level = LogLevel.Error,
+		SkipEnabledCheck = true)]
+	internal static partial void LogErrorOnChannelClose (this ILogger logger, Type worker, Exception exception, 
+		string topicName, Type topicType);
 }
